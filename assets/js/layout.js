cb27eb386c25e373be791fe5f670a1787d1f506b
sunilandharsh10 <sunilandharsh@gmail.com>
2026-05-03 11:28:34 +0530

    commit by sunil2

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -53,14 +53,9 @@ window.subMenu = function (event) {
       item.addEventListener("click", function (e) {
         if (window.innerWidth <= 768) {
           e.preventDefault();
-          
-         // item.querySelector(".submenu-content").style.display = "block";
+        
           const content = item.querySelector(".submenu");
-          // if (content.style.display === "block") {
-          //       content.style.display = "none";
-          //   } else {
-          //       content.style.display = "block";
-          //   }
+       
             this.parentElement.classList.toggle("active");
 
           
@@ -75,68 +70,3 @@ window.subMenu = function (event) {
      
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
-
-//    // Toggle main mobile menu
-//     window.toggleMenu = function () {
-//         document.getElementById("navLinks").classList.toggle("active");
-//     };
-
-//     // MAIN DROPDOWN
-//    document.querySelectorAll(".dropdown > a").forEach(function (link) {
-
-//         link.addEventListener("click", function (e) {
-
-//             if (window.innerWidth > 768) return;
-
-//             e.preventDefault();
-
-//             const parent = this.parentElement;
-
-//             // CLOSE ALL OTHER DROPDOWNS
-//             document.querySelectorAll(".dropdown").forEach(function (d) {
-//                 if (d !== parent) {
-//                     d.classList.remove("active");
-//                 }
-//             });
-
-//             // TOGGLE CURRENT
-//             parent.classList.toggle("active");
-//         });
-//     });
-
-//     // SUBMENU
-//      window.dropDown = function (event) {
-//     document.querySelectorAll(".submenu > a").forEach(link => {
-//         link.addEventListener("click", function (e) {
-
-//             if (window.innerWidth > 768) return;
-
-//             e.preventDefault();
-
-//             const parent = this.parentElement;
-//             const content = parent.querySelector(".submenu-content");
-
-//             // Close other submenus inside same dropdown
-//             parent.parentElement.querySelectorAll(".submenu").forEach(s => {
-//                 if (s !== parent) {
-//                     s.classList.remove("active");
-//                     const c = s.querySelector(".submenu-content");
-//                     if (c) c.style.display = "none";
-//                 }
-//             });
-
-//             // Toggle current submenu
-//             parent.classList.toggle("active");
-
-//             if (content.style.display === "block") {
-//                 content.style.display = "none";
-//             } else {
-//                 content.style.display = "block";
-//             }
-
-//         });
-//     });
-//   }
-
-// });
